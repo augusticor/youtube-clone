02449d8457314f5de6654d9651d7740267148113
augusticor <rojascruzoscar@gmail.com>
2023-01-25 19:55:05 -0500

    Search feed, search videos

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,25 @@
+import { useState } from 'react';
 import { IconButton, Paper } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
+import { useNavigate } from 'react-router-dom';
 
 export const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+
+  const onSearch = (e) => {
+    e.preventDefault();
+
+    if (searchTerm.length < 3) return;
+
+    navigate(`/search/${searchTerm}`);
+    setSearchTerm('');
+  };
+
   return (
     <Paper
       component='form'
-      onSubmit={() => {}}
+      onSubmit={onSearch}
       sx={{
         borderRadius: 20,
         border: '1px solid #e3e3e3',
@@ -17,8 +31,8 @@ export const SearchBar = () => {
       <input
         className='search-bar'
         placeholder='Search a video ...'
-        value=''
-        onChange={() => {}}
+        value={searchTerm}
+        onChange={(event) => setSearchTerm(event.target.value)}
       />
 
       <IconButton type='submit' sx={{ p: '10px', color: 'red' }}>
--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { Box, Typography } from '@mui/material';
+import { fetchFromAPI } from '../utils/fetchFromApi';
+import { Videos } from './Videos';
+import { useParams } from 'react-router-dom';
 
 export const SearchFeed = () => {
+  const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
+
+  useEffect(() => {
+    (async () => {
+      const data = await fetchFromAPI(`/search?part=snippet&q=${searchTerm}`);
+      setVideos(data.items);
+    })();
+  }, [searchTerm]);
+
   return (
-    <div>SearchFeed</div>
-  )
-}
+    <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+      <Typography variant='h4' fontWeight='bold' mb={2} sx={{ color: 'white' }}>
+        Search results for: <span style={{ color: '#F31503' }}>{searchTerm}</span>
+      </Typography>
+
+      <Videos videos={videos} />
+    </Box>
+  );
+};
